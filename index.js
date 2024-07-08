ac813af420a6c3e2dd62724781b79f580cec5599
pelzmagic <akinloyepelz@gmail.com>
2024-07-08 15:00:18 +0100

    contact-form

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const firstNameInput = document.querySelector(".first_name_input");
 const lastNameInput = document.querySelector(".last_name_input");
 const emailInputItem = document.querySelector(".email_input_item");
 const radioInputs = document.querySelector(".radio_inputs");
-const radioChecked = document.querySelector('input[name="query-type"]:checked');
-const consent = document.getElementById('consent_space').checked;
+const firstRadio = document.querySelector("input[name='query-type']");
+const secondRadio = document.querySelector("input[name='support_query']");
+const consent = document.getElementById('consent_space')
 // const secondRadioChecked = document.querySelector(".support_input:checked");
 const messageInput = document.querySelector(".message_input");
 const firstNameError = document.querySelector(".first_name_error");
@@ -13,6 +14,7 @@ const emailError = document.querySelector(".email_error");
 const messageError = document.querySelector(".message_error");
 const queryError = document.querySelector(".query_error");
 const consentError = document.querySelector(".consent_error");
+const successBox = document.querySelector(".success_box");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -26,22 +28,34 @@ form.addEventListener("submit", (e) => {
 
     if (firstName === '') {
         firstNameError.classList.remove("hide");
+    }else {
+        firstNameError.classList.add("hide");
     }
     if (lastName === '') {
         lastNameError.classList.remove("conceal");
+    } else {
+        lastNameError.classList.add("conceal");
     }
     if (!email.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/) || email === "") {
         emailError.classList.remove("keep");
+    } else {
+        emailError.classList.add("keep");
     }
-    if (!radioChecked) {
+    if(firstRadio.checked || secondRadio.checked) {
+        queryError.classList.add("hoard");
+    } else {
         queryError.classList.remove("hoard");
     }
     if (message === "") {
         messageError.classList.remove("lock");
         messageInput.style.borderColor = "red";
     }
-    // if (!consent) {
-    //     isValid = false;
-    //     consentError.classList.remove("bury");
-    // }
-})
+    if (consent.checked) {
+        consentError.classList.add("bury");
+    } else {
+        consentError.classList.remove("bury");
+    }
+    if (firstName !== "" && lastName !== "" && email !== "" && email.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/) && firstRadio.checked && secondRadio.checked && consent.checked && message !== "") {
+        successBox.classList.remove("cover");
+    }
+});
